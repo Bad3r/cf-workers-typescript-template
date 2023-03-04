16566c0507bc2592cd4c3f304a86abd7b19acf1f
Bad3r <bad3r@protonmail.com>
2023-03-03 22:43:38 -0500

    Update App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App" style={{backgroundColor: "black", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
-      <h1 style={{color: "white", fontSize: "4rem", fontFamily: "ML"}}>Hello, Friend</h1>
+      <h1 style={{color: "white", fontSize: "4rem", fontFamily: "ML"}}>Hello, Friend.</h1>
     </div>
   );
 }
