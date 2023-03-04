b4ea0cd369560fd8768443ea64ac0bf545c81d94
Bad3r <bad3r@protonmail.com>
2023-03-03 22:38:21 -0500

    Update App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,12 @@
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello, {this.props.name}</div>;
-  }
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: "black", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      <h1 style={{color: "white", fontSize: "4rem", fontFamily: "ML"}}>Hello, Friend</h1>
+    </div>
+  );
 }
 
-root.render(<HelloMessage name="Friend." />);
+export default App;
